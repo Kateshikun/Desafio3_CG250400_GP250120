54cc5c2dd65d89dbd4d6de7c8835ce34f1e284a7
Kateshikun <danieladriancastillogarcia@gmail.com>
2025-10-16 22:09:18 -0600

    Agregar archivos de proyecto.

---
+++ Desafio3 CG250400_GP250120/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desafio3_CG250400_GP250120
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
